9157a13a2abb09b3360418fed75f96fb97b85224
LuisFernandoSantos <32277920+luisipv7@users.noreply.github.com>
2019-03-07 01:31:36 -0300

    Add files via upload

---
+++ functions-arguments.js
@@ -0,0 +1,11 @@
+function math(firstArg,secondArg,thirdArg) {
+    var resultmult,resulttotal;
+    //soma segundo com terceiro
+    resultmult = secondArg*thirdArg;
+    resulttotal = resultmult + firstArg;
+
+    return resulttotal;
+    
+}
+
+console.log(math(53,61,67));
